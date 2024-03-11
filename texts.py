e029f73258e437e11349c6ee01c8dcb28cbcce5d
thjbbvlt <thibault2@ik.me>
2024-03-11 22:41:52 +0100

    séparé tokenizer (mecanique) et frenchtokenizer

---
+++ texts.py
@@ -0,0 +1,59 @@
+hyphens = """
+Où vas-tu? 
+Où va-t'on? 
+Je ne sais pas mais on y va depuis le 2023-12-21.
+Qu'en est-t-il des autres?
+Sont-iels à Village-sur-Rivière?
+À Maison-en-forêt?
+À Cabane-la-magique?
+Et les non-humain-es?
+Et le produit anti-nominaliste?
+T'en as? Tu m'en donnnes?
+Sinon mets-en un peu par terre.
+"""
+
+apostrophes = """
+il n'y a plus rien ici. enfin j'crois, nos p'tites affaires ont été enl'vées.
+aujourd'hui, c'est comme ça.
+"""
+
+period_digit = """
+Les numéros 12.1 et 132.121, et 1.213 et 1.2.3, aussi le 1.a. 
+Alors lisez déjà la p.30, la p.3, la p.5 la p. 20 et la p.2.
+Ah et le 2.3! Pas le 2.par contre.3 oui 3 aussi. 
+Éventuellement le numéro 4.
+Le 10.a.2 et A.3.a pourquoi pas.
+On est quoi, le 21.12.2023...
+"""
+
+comma = """
+2,3 et non,2 c'est pas assez,3 non plus et 4,je sais pas.
+"""
+
+slash = """
+ici il n'y rien/pas grand chose. 1/mais par contre on s'amuse.
+et le 20.12, ou alors le 21/12 oui c'est ça c'était le 21/12/2023...
+"""
+
+parenthese = """
+(oui) le(s) tableau(x) est là.(dé)peins-le, nan?
+(et où sont le(s) reste(s))
+quelqu'un.e a dit "[que] les personnes [s]e promène[nt]"
+et elles se promen(erai)ent.
+"""
+
+ecriture_inclusive = """
+un.e directeur.ice, des employé.es, des juriste.x.s.
+tout le monde était là.Et à l'heure!et.à.l'heure!
+"""
+
+url = """
+j'ai trouvé ça surhttps://site_inexistant.fr/page_accueil#milieu 
+mais aussi on doit trouver des trucs sur www.quelque_part.com/
+sinon sur l'ordinateur centrale, 
+tu cherche le fichier /home/invite/paysages/foret.pdf. 
+Et vers la bas de l'image tu la vois, la grenouille magique à qui on 
+peut écrire à l'adresse mailto:grenouille@magique.com
+Et son site perso est soit gemini://grenouille.magique soit gopher://grenouille.magique!
+:-)
+"""
