15a35cf4b43882a06507609ba0a6be22a0f77284
thjbbvlt <thibault2@ik.me>
2024-03-11 22:02:40 +0100

    commented main 'tokenizer' function

--- tokenizer.py
+++ tokenizer.py
@@ -23,9 +23,11 @@ class Tokenizer:
         self.abbrev = abbrev
 
     def tokenize(self, text):
+        """split a text into tokens."""
         re_splitspace = self.re_splitspace
         re_freeze = self.re_freeze
         re_splitpunct = self.re_splitpunct
+        # split on spaces. and mark all spaces as spaces.
         spacesplitted = re_splitspace(text)
         spacesplitted = [
             Token(text=i[0], isspace=i[1])
@@ -33,24 +35,35 @@ class Tokenizer:
                 spacesplitted, [False, True] * len(spacesplitted)
             )
         ]
+        # remove empty token at start and ends, if there are some (which is the case when first or last string is a space).
+        if len(spacesplitted) > 0 and spacesplitted[0].text == "":
+            spacesplitted = spacesplitted[1:]
+        if len(spacesplitted) > 0 and spacesplitted[-1].text == "":
+            spacesplitted = spacesplitted[:-1]
+        # initiate a document, in which tokens will be put.
         doc = []
         for substring in spacesplitted:
             if substring.isspace is True:
                 doc.append(substring)
             else:
+                # get positions of punctuation sign that might split tokens.
                 puncts = re_splitpunct(substring.text)
-                s = []
-                for i in puncts:
-                    s.extend([i.start(), i.end()])
-                s = set(s)
+                s = set().union(
+                    *[(i.start(), i.end()) for i in puncts]
+                )
+                # and remove from these numerical positions those which are marked as 'frozen' (exception).
                 frozen = re_freeze(substring.text)
-                for i in frozen:
-                    s.difference_update(range(i.start(), i.end()))
+                s.difference_update(
+                    *[range(i.start(), i.end()) for i in frozen]
+                )
                 if len(s) == 0:
+                    # if no split-punct remains, append substring as-is
                     doc.append(substring)
                 else:
+                    # else, add all parts one after the other.
+                    # add 0 and len(substring.text) to ensure all text is kept.
+                    s.update([0, len(substring.text)])
                     x = sorted(s)
-                    x = [0] + x + [len(substring.text)]
                     for n, i in enumerate(x[:-1]):
                         doc.append(
                             Token(
@@ -223,7 +236,7 @@ class FrenchTokenizer(Tokenizer):
 
     def _aggregex_freeze(self):
         regex_freeze = [
-            self._genregex_abbrev_singleletter(),  # arg ici souci!!
+            self._genregex_abbrev_singleletter(),
             self._genregex_digitpunct(),
             self._genregex_inword_parenthese(),
         ]
