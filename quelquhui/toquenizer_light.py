ea251a4c169064c77027ed9d6435e8d7e8adc25c
thjbbvlt <thibault2@ik.me>
2024-05-01 19:21:48 +0200

    fonctionne (pas incroyable)

--- quelquhui/toquenizer_light.py
+++ quelquhui/toquenizer_light.py
@@ -15,22 +15,36 @@ class QQHuiToquenizer:
 
     def itersplit(self, text: str):
         # non ça ne va pas du tout, c'est beauuuucoup trop long
+        # ha mais je testais mal, je reesaie
         s = [
-            (i.start(), i.end())
+            (i.start(), i.end(), False)
             for i in self.splitspace(text)
         ]
+        for fn in self.splitwords:
+            for n, token in enumerate(s):
+                start, end, frozen = token
+                substring = text[start:end]
+                x = list(fn.finditer(substring))
+                if len(x) == 0:
+                    s[n] = [token]
+                    continue
+                matches = [(i.start()+start, i.end()+start, True) for i in x]
+                nonmatches = [(matches[n][1], matches[n+1][0], False) for n in range(len(matches)-1)] + [(start, matches[0][0], False), (matches[-1][1], end, False)]
+                s[n] = sorted(matches + nonmatches)
+            s = [x for y in s for x in y]
         return s
 
     def tokenize(self, text: str) -> list[tuple[int, int]]:
         """split a text into tokens."""
         # three functions that do the job by freezing and splitting.
-        re_nonspace = self.splitspace
         re_splitpunct = self.findborder
         re_freeze = self.findfreeze
         d = []
-        for nonspace in re_nonspace(text):
-            start = nonspace.start()
-            end = nonspace.end()
+        for nonspace in self.itersplit(text):
+            start, end, frozen = nonspace
+            if frozen is True:
+                d.append((start, end))
+                continue
             substring = text[start:end]
 
             # get positions of punctuation signs that might split tokens.
