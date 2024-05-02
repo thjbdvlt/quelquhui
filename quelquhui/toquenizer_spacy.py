f58feb11b3f6be381713b7b2f3e5cec668e77bb9
thjbbvlt <thibault2@ik.me>
2024-05-02 18:53:33 +0200

    works

--- quelquhui/toquenizer_spacy.py
+++ quelquhui/toquenizer_spacy.py
@@ -4,6 +4,19 @@ from typing import Callable, Iterable
 from quelquhui.itersplit import alternatefalsetrue, infinitefalse
 
 
+def wherespaces(spacesidx: list[int], words: list[str]):
+    n = idx = 0
+    spaces = []
+    lo = [len(i) for i in words]
+    for i in lo:
+        idx += i
+        if spacesidx[n] == idx:
+            spaces.append(True)
+        else:
+            spaces.append(False)
+    return spaces
+
+
 class QQSpacyToquenizer:
     def __init__(
         self,
@@ -20,26 +33,48 @@ class QQSpacyToquenizer:
         self.splitspace = splitspace
         self.splitpatterns = splitwords
         self.findborder = findborder
-        self.freeze = findfreeze
+        self.findfreeze = findfreeze
 
-    def itersplit(self, text: str) -> list[str]:
-        """split itérativement un texte à l'aide d'une liste de fonction."""
+    def itersplit(self, word: list[tuple]) -> list[str]:
+        """split itérativement un mot à l'aide d'une liste de fonction."""
 
-        # split d'abord sur les espaces, sans les conserver. au départ, aucun mot n'est gelé au départ.
-        s = zip(self.splitspace(text), infinitefalse())
         # itération sur les fonctions de splitting. l'ordre est important: une fois qu'un élément extrait est extrait comme étant un token par l'une des fonctions, les fonctions suivantes ne le modifieront plus (le token est gelé).
+        words = [word]
         for fn in self.splitpatterns:
-            split = fn.split
-            search = fn.search
-            s = (
+            search, split = fn.search, fn.split
+            words = (
                 zip(split(i[0]), alternatefalsetrue())
                 if i[1] is False and search(i[0])
                 else [i]
-                for i in s
+                for i in words
             )
             # unnest la nested list et enlève les éléments vides
-            s = [x for y in s for x in y if x[0] != ""]
-        return s
+            words = [x for y in words for x in y if x[0] != ""]
+        return words
+
+    def findsplit(self, substring: str) -> list[str]:
+        """split a substring into many using two functions: one that find potential boundaries, and another one that find some exceptions that will be substract for the boundaries."""
+        # find borders
+        s = set().union(
+            *[(i.start(), i.end()) for i in self.findborder(substring)]
+        )
+        # find exceptions, and remove exceptions from borders
+        s.difference_update(
+            *[
+                range(i.start(), i.end())
+                for i in self.findfreeze(substring)
+            ]
+        )
+        if len(s) == 0:
+            # if no borders remains, return substring without any change
+            return [substring]
+        # else, add all parts one after the other. add 0 and len(substring.text) to ensure all text is kept.
+        s.update([0, len(substring)])
+        x = sorted(s)
+        words = [
+            substring[i : x[n + 1]] for n, i in enumerate(x[:-1])
+        ]
+        return words
 
     def tokenize(self, text: str, **kwargs) -> Doc:
         # 1. split text on spaces, then re-split with self.split functions.
@@ -47,36 +82,20 @@ class QQSpacyToquenizer:
         #    2.1 find characters to split on.
         #    2.2 find characters found in 2.1 but not to split on.
         #    2.3 split on 2.1 - 2.2
-        freeze = self.freeze
-        findborder = self.findborder
+        # freeze = self.freeze
+        # findborder = self.findborder
         # split on spaces, then eventually split on url, then emoji, then emoticon (or using other split rules submitted in split_patterns).
-        words = self.itersplit(text)
-        for idx, (substring, isfrozen) in enumerate(words):
-            if isfrozen is True:
-                words[idx] = [substring]
-                continue
-            # get positions of chars that might split tokens.
-            s = set().union(
-                *[(i.start(), i.end()) for i in findborder(substring)]
-            )
-            # and remove from these numerical positions those which are marked as 'frozen' (exception).
-            s.difference_update(
-                *[
-                    range(i.start(), i.end())
-                    for i in freeze(substring)
-                ]
-            )
-            if len(s) == 0:
-                # if no split-punct remains, append substring as-is
-                words[idx] = [substring]
-                continue
-            # else, add all parts one after the other. add 0 and len(substring.text) to ensure all text is kept.
-            s.update([0, len(substring)])
-            x = sorted(s)
-            words[idx] = [
-                substring[i : x[n + 1]] for n, i in enumerate(x[:-1])
-            ]
-        # vérifier ici que ça fonctionne: je suis pas sûr!!!
+        nonspace = list(zip(self.splitspace(text), infinitefalse()))
+        # si le dernier token est "", alors c'est que le texte termine par un espace. cela nécessite quelques ajustements pour la suite
+        if nonspace[-1][0] == "":
+            nonspace = nonspace[:-1]
+            nonspace[-1] = (nonspace[-1][0], True)
+        words = []
+        for i in nonspace:
+            x = self.itersplit(i)
+            subwords = [self.findsplit(word[0]) if word[1] is False else [word[0]] for word in x]
+            words.append([a for u in subwords for a in u])
+        # créer une liste qui dit si les mots sont suivis ou non par des espaces.
         spaces = [[False] * (len(i) - 1) + [True] for i in words]
         # unnest both lists: first, spaces.
         spaces = [x for y in spaces for x in y]
@@ -84,15 +103,10 @@ class QQSpacyToquenizer:
         # to avoid error. returns empty docs before the end of the processing.
         if len(words) == 0:
             return Doc(words=[], spaces=[], vocab=self.vocab)
-        # if there is an empty token, then the string ends with a space. i remove the empty token, but i keep 'True' as the last values in 'spaces'. else, i change last value to 'False' because the last token isn't followed by a space.
-        elif words[-1] == "":
-            words = words[:-1]
-            spaces = spaces[:-1]
         else:
-            spaces[-1] = False
-        return Doc(
-            words=words, spaces=spaces, vocab=self.vocab, **kwargs
-        )
+            return Doc(
+                words=words, spaces=spaces, vocab=self.vocab, **kwargs
+            )
 
     def __call__(self, text: str, **kwargs) -> Doc:
         return self.tokenize(text, **kwargs)
