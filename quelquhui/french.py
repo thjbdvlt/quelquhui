1797a54db3ab72f8817bb6d326cde08234492e25
thjbbvlt <thibault2@ik.me>
2024-05-19 14:16:40 +0200

    ruff format

--- quelquhui/french.py
+++ quelquhui/french.py
@@ -171,7 +171,7 @@ class French:
         # aggregate the 'come after' groups
         if_group_then = rf"(?(f){if_f}|(?(x){if_x}|{if_s}))"
 
-        self.inclusive_suffix = fr"({firstsuffix}({if_group_then}))"  # pour d'autres utilisation à l'aide de 'fullmatch'. nécessite de ne pas utiliser (?=) à la fin (car alors impossible de fullmatch une string qui doit être suivi par qqch).
+        self.inclusive_suffix = rf"({firstsuffix}({if_group_then}))"  # pour d'autres utilisation à l'aide de 'fullmatch'. nécessite de ne pas utiliser (?=) à la fin (car alors impossible de fullmatch une string qui doit être suivi par qqch).
 
         return rf"(?<=.){chars}({firstsuffix}(?={if_group_then}))"
 
